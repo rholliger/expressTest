84758ae9b00e851b9fa0d8e374636476db94fc80
Roy Holliger <roy@holliger.li>
2016-10-12 20:50:07 +0200

    This goes live!

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Roy ist der Beste!' });
+  res.render('index', { title: 'Endlich ist die Arbeit getan' });
 });
 
 module.exports = router;
