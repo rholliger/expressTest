21ef3f7d3562ff60e783fb59c230ef7b156b0f79
Roy Holliger <roy@holliger.li>
2016-10-12 20:06:39 +0200

    Another try

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    console.log("Do another try");
   res.render('index', { title: 'Express' });
 });
 
